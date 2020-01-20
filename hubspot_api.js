86f7861869e59141c9a59d7e5a897ae24bceb2f7
Sonia Rottenberg <sonia.rottenberg@gmail.com>
2020-01-20 18:36:12 +0100

    npm and package generated

--- hubspot_api.js
+++ hubspot_api.js
@@ -1,4 +1,36 @@
-const request = require("request")
+var request = require("request")
 const returnedCompanies = [];
-const API_KEY = 'demo'
+const API_KEY = '3b027ac0-0ae1-489f-b424-822c3df0b024'
 const count = 5;
+
+getCompanies = () => {
+    if (typeof offset == 'undefined') {
+        offsetParam = null;
+    } else {
+        offsetParam = `offset=${offset}`;
+    }
+
+    const hapikeyParam = `hapikey=${API_KEY}`
+    const paramsString = `?count=${count}&${hapikeyParam}&${offsetParam}`;
+
+    const finalUrl = `https://api.hubapi.com/companies/v2/companies/paged${paramsString}`
+    console.log(finalUrl)
+    request(finalUrl, (error, response, body) => {
+        if (error) {
+            console.log('error', error)
+            throw new Error
+        }
+        const parsedBody = JSON.parse(body)
+        parsedBody.companies.forEach(company => {
+            returnedCompanies.push(company);
+        });
+        if (parsedBody['has-more']) {
+            getCompanies(parsedBody['offset'])
+        } else {
+            //print out all companies
+            console.log(returnedCompanies)
+        }
+    })
+}
+
+getCompanies();
